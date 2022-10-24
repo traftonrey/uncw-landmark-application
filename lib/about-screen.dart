2cf475faf348dca9256eeb3f29a494776471b4bd
Trafton <reynoldstrafton@gmail.com>
2022-10-23 20:17:04 -0400

    cleaned up main and created about-screen.dart

---
+++ lib/about-screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:uncw_landmark_app/main.dart';
+
+class AboutScreen extends StatelessWidget {
+  AboutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
