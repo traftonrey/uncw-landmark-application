e421f4d3514a9709a2abc4c2fb3ee37787988663
AndrewBracero <a.bracero777@gmail.com>
2022-11-06 18:41:05 -0500

    Created site class in site_data.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:uncw_landmark_app/about-screen.dart';
+import 'package:uncw_landmark_app/about_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'loginscreen.dart';
---
+++ lib/site_data.dart
@@ -0,0 +1,8 @@
+class Site {
+  const Site(
+      {required this.name, required this.description, required this.reference});
+  
+  final String name;
+  final String description;
+  final String reference;
+}
