2c3d83efd8fb5638b1359ba04b266e6ae184084f
Trafton <reynoldstrafton@gmail.com>
2022-11-06 18:43:33 -0500

    login and signup screens

---
+++ lib/login-signup-screen.dart
@@ -0,0 +1,265 @@
+import 'package:flutter/material.dart';
+import 'package:uncw_landmark_app/about-screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'main.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String? email;
+  String? password;
+  String? error;
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Spacer(flex: 3),
+              TextFormField(
+                  decoration:
+                      const InputDecoration(hintText: 'Enter your email'),
+                  maxLength: 64,
+                  onChanged: (value) => email = value,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null; // Returning null means "no issues"
+                  }),
+              TextFormField(
+                  decoration:
+                      const InputDecoration(hintText: "Enter a password"),
+                  obscureText: true,
+                  onChanged: (value) => password = value,
+                  validator: (value) {
+                    if (value == null || value.length < 8) {
+                      return 'Your password must contain at least 8 characters.';
+                    }
+                    return null; // Returning null means "no issues"
+                  }),
+              const Spacer(flex: 2),
+              ElevatedButton(
+                  child: const Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 15.0, horizontal: 100),
+                    child: Text('Login'),
+                  ),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      // This calls all validators() inside the form for us.
+                      tryLogin();
+                    }
+                  }),
+              if (error != null)
+                Text(
+                  "Error: $error",
+                  style: TextStyle(color: Colors.red[800], fontSize: 12),
+                ),
+              const Spacer(flex: 3),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pushAndRemoveUntil(
+                      MaterialPageRoute(
+                          builder: (context) => const SignUpScreen()),
+                      (route) => false);
+                },
+                child: const Text("Sign-Up Page"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void tryLogin() async {
+    try {
+      // The await keyword blocks execution to wait for
+      // signInWithEmailAndPassword to complete its asynchronous execution and
+      // return a result.
+      //
+      // FirebaseAuth with raise an exception if the email or password
+      // are determined to be invalid, e.g., the email doesn't exist.
+      final credential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email!, password: password!);
+      print("Logged in ${credential.user}");
+      error = null; // clear the error message if exists.
+      setState(() {}); // Call setstate to trigger a rebuild
+
+      // We need this next check to use the Navigator in an async method.
+      // It basically makes sure LoginScreen is still visible.
+      if (!mounted) return;
+
+      // pop the navigation stack so people cannot "go back" to the login screen
+      // after logging in.
+      Navigator.of(context).pop();
+      // Now go to the HomeScreen.
+      Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => const HomeScreen(),
+      ));
+    } on FirebaseAuthException catch (e) {
+      // Exceptions are raised if the Firebase Auth service
+      // encounters an error. We need to display these to the user.
+      if (e.code == 'user-not-found') {
+        error = 'No user found for that email.';
+      } else if (e.code == 'wrong-password') {
+        error = 'Wrong password provided for that user.';
+      }
+
+      // Call setState to redraw the widget, which will display
+      // the updated error text.
+      setState(() {});
+    }
+  }
+}
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  String? email;
+  String? password;
+  String? repeatPassword;
+  String? error;
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Spacer(flex: 3),
+              TextFormField(
+                  decoration:
+                      const InputDecoration(hintText: 'Enter your email'),
+                  maxLength: 64,
+                  onChanged: (value) => email = value,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null; // Returning null means "no issues"
+                  }),
+              TextFormField(
+                  decoration:
+                      const InputDecoration(hintText: "Enter a password"),
+                  obscureText: true,
+                  onChanged: (value) => password = value,
+                  validator: (value) {
+                    if (value == null || value.length < 8) {
+                      return 'Your password must contain at least 8 characters.';
+                    }
+                    return null; // Returning null means "no issues"
+                  }),
+              const SizedBox(height: 20),
+              TextFormField(
+                  decoration:
+                      const InputDecoration(hintText: "Repeat password"),
+                  obscureText: true,
+                  onChanged: (value) => repeatPassword = value,
+                  validator: (value) {
+                    if (value == null || value.length < 8) {
+                      return 'Your password must contain at least 8 characters.';
+                    }
+                    return null; // Returning null means "no issues"
+                  }),
+              const Spacer(flex: 2),
+              ElevatedButton(
+                  child: const Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 15.0, horizontal: 100),
+                    child: Text('Sign Up'),
+                  ),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      // This calls all validators() inside the form for us.
+                      trySignup();
+                    }
+                  }),
+              if (error != null)
+                Text(
+                  "Error: $error",
+                  style: TextStyle(color: Colors.red[800], fontSize: 12),
+                ),
+              const Spacer(flex: 3),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pushAndRemoveUntil(
+                      MaterialPageRoute(
+                          builder: (context) => const LoginScreen()),
+                      (route) => false);
+                },
+                child: const Text("Login Page"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void trySignup() async {
+    try {
+      // The await keyword blocks execution to wait for
+      // signInWithEmailAndPassword to complete its asynchronous execution and
+      // return a result.
+      //
+      // FirebaseAuth with raise an exception if the email or password
+      // are determined to be invalid, e.g., the email doesn't exist.
+      final credential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email!, password: password!);
+      print("Created account ${credential.user}");
+      error = null; // clear the error message if exists.
+      setState(() {}); // Call setstate to trigger a rebuild
+
+      // We need this next check to use the Navigator in an async method.
+      // It basically makes sure LoginScreen is still visible.
+      if (!mounted) return;
+
+      // pop the navigation stack so people cannot "go back" to the login screen
+      // after logging in.
+      Navigator.of(context).pop();
+      // Now go to the HomeScreen.
+      Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => const HomeScreen(),
+      ));
+    } on FirebaseAuthException catch (e) {
+      // Exceptions are raised if the Firebase Auth service
+      // encounters an error. We need to display these to the user.
+      if (e.code == 'user-not-found') {
+        error = 'No user found for that email.';
+      } else if (e.code == 'wrong-password') {
+        error = 'Wrong password provided for that user.';
+      } else if (password != repeatPassword) {
+        error = 'Passwords do not match. Please try again.';
+      }
+
+      // Call setState to redraw the widget, which will display
+      // the updated error text.
+      setState(() {});
+    }
+  }
+}
