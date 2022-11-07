ec70dfc2d9ae3d7fee960b63790f327993fa0d4b
AndrewBracero <a.bracero777@gmail.com>
2022-11-06 19:58:56 -0500

    completed list of site data

--- lib/site_data.dart
+++ lib/site_data.dart
@@ -8,22 +8,56 @@ class Site {
 }
 
 final List<Site> sites = [
-  Site(
+  const Site(
     name: "Cogdon Hall",
     description:
         "Cogdon Hall hosts the computer science department and Management Information Systems program of the Cameron School of Business.",
     reference: "CIS",
   ),
-  Site(
+  const Site(
     name: "Dobo Hall",
     description:
         "Dobo Hall hosts the Marine Biology, Biology, Chemistry, and Biochemistry departments",
     reference: "dobo_hall",
   ),
-  Site(
+  const Site(
     name: "Fisher Student Center",
     description:
         "Fisher student center hosts campus actvities and involvement centers",
     reference: "fisher_student_center",
+  ),
+  const Site(
+    name: "Fisher Union",
+    description:
+        "Fisher Union hosts several campus dining options and campus organizations",
+    reference: "fisher_union",
+  ),
+  const Site(
+    name: "Pelican Hall",
+    description: "Pelican Hall is one of new buildings for student living",
+    reference: "pelican_hall",
+  ),
+  const Site(
+    name: "Sandpiper Hall",
+    description:
+        "Sandpiper Hall is one of the new buildings for student living",
+    reference: "sandpiper_hall",
+  ),
+  const Site(
+    name: "Shore Dining Hall",
+    description:
+        "Shore Dining is UNCW's most recent addition to the options available for student dining",
+    reference: "shore_dining",
+  ),
+  const Site(
+    name: "Recreation Center",
+    description:
+        "The Recreation Center hosts the UNCW gym, basketball courts, wall climbing, pools, and other recreational sports and classes",
+    reference: "rec_center",
+  ),
+  const Site(
+    name: "Veterans Hall",
+    description: "Veterans Hall hosts the Human Health and Services College",
+    reference: "veterans_hall",
   ),
 ];
