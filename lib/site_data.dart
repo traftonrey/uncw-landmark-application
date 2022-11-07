7b9b91a4452ecf462c7f85c06ff68bfda1100894
AndrewBracero <a.bracero777@gmail.com>
2022-11-06 19:48:15 -0500

    Created half the list of sites

--- lib/site_data.dart
+++ lib/site_data.dart
@@ -1,8 +1,29 @@
 class Site {
   const Site(
       {required this.name, required this.description, required this.reference});
-  
+
   final String name;
   final String description;
   final String reference;
 }
+
+final List<Site> sites = [
+  Site(
+    name: "Cogdon Hall",
+    description:
+        "Cogdon Hall hosts the computer science department and Management Information Systems program of the Cameron School of Business.",
+    reference: "CIS",
+  ),
+  Site(
+    name: "Dobo Hall",
+    description:
+        "Dobo Hall hosts the Marine Biology, Biology, Chemistry, and Biochemistry departments",
+    reference: "dobo_hall",
+  ),
+  Site(
+    name: "Fisher Student Center",
+    description:
+        "Fisher student center hosts campus actvities and involvement centers",
+    reference: "fisher_student_center",
+  ),
+];
