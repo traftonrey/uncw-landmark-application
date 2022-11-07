31e7af82df0b70ada385583c9d7b999e0b7363fd
AndrewBracero <a.bracero777@gmail.com>
2022-11-06 19:16:25 -0500

    added images to assets

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:uncw_landmark_app/about_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'loginscreen.dart';
+import 'site_data.dart';
 
 void main() async {
   runApp(const MaterialApp(title: "Firebase Example", home: LoginScreen()));
